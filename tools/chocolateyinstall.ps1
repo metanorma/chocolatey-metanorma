e6a36222e8a0775b9e6dcdc4742680e3f6f397d0
Alexande B <abobrikovich@gmail.com>
2018-12-16 01:40:01 +0300

    Specify right ruby distribution for appveyor CI

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,35 +1,28 @@
-﻿New-Item -ItemType file -Force $Env:ChocolateyInstall\bin\xml2-config.bat
-New-Item -ItemType file -Force $Env:ChocolateyInstall\bin\xslt-config.bat
+﻿Set-Content $Env:ChocolateyInstall\bin\xml2-config.bat "@ECHO OFF" -Encoding ASCII
+Set-Content $Env:ChocolateyInstall\bin\xslt-config.bat "@ECHO OFF" -Encoding ASCII
 
 $XsltDist = ${Env:ChocolateyInstall} + "\lib\xsltproc\dist"
 $XsltInclude = $XsltDist + "\include"
 $XsltLib = $XsltDist + "\lib"
-$RubyBin = "c:\tools\ruby25\bin"
+$RubyGem = "c:\tools\ruby25\bin"
 
 Write-Host Installing gems...
-
-Push-Location $RubyBin
-gem install bundler 
-gem install metanorma-cli -v ${Env:ChocolateyPackageVersion} -- `
-  --with-xml2-include=$XsltInclude\libxml2 `
-  --with-xslt-include=$XsltInclude `
-  --with-xml2-lib=$XsltLib `
-  --with-xslt-lib=$XsltLib
-Pop-Location
-
-& SETX /M RUBY_DLL_PATH "${Env:ChocolateyInstall}\lib\xsltproc\dist\bin;${env:RUBY_DLL_PATH}"
-refreshenv
 
 # Copy with removing version from filename (need because xslt_lib.so expect such names)
 Copy-Item -Force $XsltDist\bin\libxml2-*.dll $XsltDist\bin\libxml2.dll 
 Copy-Item -Force $XsltDist\bin\libxslt-*.dll $XsltDist\bin\libxslt.dll
 Copy-Item -Force $XsltDist\bin\libexslt-*.dll $XsltDist\bin\libexslt.dll
 
-Write-Host List applications...
-Get-Command ruby
-Get-Command gem
-Get-Command bundle
-Get-Command java
-Get-Command metanorma
+& $RubyGem\gem.cmd -v
+& $RubyGem\gem.cmd install bundler
+& $RubyGem\gem.cmd install metanorma-cli -v $Env:ChocolateyPackageVersion -- `
+  --with-xml2-include=$XsltInclude\libxml2 `
+  --with-xslt-include=$XsltInclude `
+  --with-xml2-lib=$XsltLib `
+  --with-xslt-lib=$XsltLib
 
-metanorma --help
+& SETX /M RUBY_DLL_PATH "${Env:ChocolateyInstall}\lib\xsltproc\dist\bin;${Env:RUBY_DLL_PATH}"
+refreshenv
+
+Write-Host Checking metanorma-cli
+Get-Command metanorma | Select-Object -ExpandProperty Definition
