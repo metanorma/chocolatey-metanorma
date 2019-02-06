6c7c64b44d09344fd793c9bb50696c1a25070ad3
Alexande B <abobrikovich@gmail.com>
2019-02-06 22:18:48 +0300

    Add msys2 as dependency to avoid ridk call

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,17 +5,24 @@ $XsltDist = ${Env:ChocolateyInstall} + "\lib\xsltproc\dist"
 $XsltInclude = $XsltDist + "\include"
 $XsltLib = $XsltDist + "\lib"
 
-Write-Host Installing gems...
-
-# Copy with removing version from filename (need because xslt_lib.so expect such names)
-Copy-Item -Force $XsltDist\bin\libxml2-*.dll $XsltDist\bin\libxml2.dll 
-Copy-Item -Force $XsltDist\bin\libxslt-*.dll $XsltDist\bin\libxslt.dll
-Copy-Item -Force $XsltDist\bin\libexslt-*.dll $XsltDist\bin\libexslt.dll
+Write-Host Installing puppeteer...
 
 & npm i -g puppeteer
 
-Get-ToolsLocation
+Write-Host Installing RIDK...
+
 $RubyGem = "$Env:ChocolateyToolsLocation\ruby25\bin"
+$RidkProcess = Start-Process -PassThru -FilePath "$RubyGem\ridk.cmd" -ArgumentList "install 2 3"
+# Start-Process prevent Appveyour from hang
+Wait-Process -Id $RidkProcess.Id
+Update-SessionEnvironment
+
+Write-Host Installing gems...
+
+# Copy with removing version from filename (need because xslt_lib.so expect such names)
+Copy-Item -Force $XsltDist\bin\libxml2-*.dll $XsltDist\bin\libxml2.dll
+Copy-Item -Force $XsltDist\bin\libxslt-*.dll $XsltDist\bin\libxslt.dll
+Copy-Item -Force $XsltDist\bin\libexslt-*.dll $XsltDist\bin\libexslt.dll
 
 & $RubyGem\gem.cmd -v
 & $RubyGem\gem.cmd install bundler
@@ -25,8 +32,8 @@ $RubyGem = "$Env:ChocolateyToolsLocation\ruby25\bin"
   --with-xml2-lib=$XsltLib `
   --with-xslt-lib=$XsltLib
 
-& SETX /M RUBY_DLL_PATH "${Env:ChocolateyInstall}\lib\xsltproc\dist\bin;${Env:RUBY_DLL_PATH}"
-refreshenv
+[Environment]::SetEnvironmentVariable("RUBY_DLL_PATH", "${Env:ChocolateyInstall}\lib\xsltproc\dist\bin;${Env:RUBY_DLL_PATH}", [System.EnvironmentVariableTarget]::Machine)
+Update-SessionEnvironment
 
 Write-Host Checking metanorma-cli
 Get-Command metanorma | Select-Object -ExpandProperty Definition
