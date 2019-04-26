7ac4e88969f7f87828ed4cfec2413476c443914d
Alexande B <abobrikovich@gmail.com>
2019-04-26 23:50:30 +0300

    Release 1.1.7

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -2,7 +2,3 @@
 
 $RubyGem = "$Env:ChocolateyToolsLocation\ruby25\bin"
 & $RubyGem\gem.cmd uninstall --all -x metanorma-cli
-
-$RUBY_DLL_PATH = $Env:RUBY_DLL_PATH.Replace("${Env:ChocolateyInstall}\lib\xsltproc\dist\bin;", "")
-[Environment]::SetEnvironmentVariable("RUBY_DLL_PATH", $RUBY_DLL_PATH, [System.EnvironmentVariableTarget]::Machine)
-Update-SessionEnvironment
