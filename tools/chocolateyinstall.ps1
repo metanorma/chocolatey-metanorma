d83d06e8105a094993ba0f7310191b34ea4276a0
Alexande B <abobrikovich@gmail.com>
2019-04-26 23:50:30 +0300

    Release 1.1.7

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,3 @@
-Set-Content $Env:ChocolateyInstall\bin\xml2-config.bat "@ECHO OFF" -Encoding ASCII
-Set-Content $Env:ChocolateyInstall\bin\xslt-config.bat "@ECHO OFF" -Encoding ASCII
-
-$XsltDist = ${Env:ChocolateyInstall} + "\lib\xsltproc\dist"
-$XsltInclude = $XsltDist + "\include"
-$XsltLib = $XsltDist + "\lib"
-
 Write-Host Installing puppeteer...
 
 & npm i -g puppeteer
@@ -19,21 +12,8 @@ Update-SessionEnvironment
 
 Write-Host Installing gems...
 
-# Copy with removing version from filename (need because xslt_lib.so expect such names)
-Copy-Item -Force $XsltDist\bin\libxml2-*.dll $XsltDist\bin\libxml2.dll
-Copy-Item -Force $XsltDist\bin\libxslt-*.dll $XsltDist\bin\libxslt.dll
-Copy-Item -Force $XsltDist\bin\libexslt-*.dll $XsltDist\bin\libexslt.dll
-
-& $RubyGem\gem.cmd -v
 & $RubyGem\gem.cmd install bundler
-& $RubyGem\gem.cmd install metanorma-cli -v $Env:ChocolateyPackageVersion -- `
-  --with-xml2-include=$XsltInclude\libxml2 `
-  --with-xslt-include=$XsltInclude `
-  --with-xml2-lib=$XsltLib `
-  --with-xslt-lib=$XsltLib
-
-[Environment]::SetEnvironmentVariable("RUBY_DLL_PATH", "${Env:ChocolateyInstall}\lib\xsltproc\dist\bin;${Env:RUBY_DLL_PATH}", [System.EnvironmentVariableTarget]::Machine)
-Update-SessionEnvironment
+& $RubyGem\gem.cmd install metanorma-cli -v $Env:ChocolateyPackageVersion
 
 Write-Host Checking metanorma-cli
 Get-Command metanorma | Select-Object -ExpandProperty Definition
