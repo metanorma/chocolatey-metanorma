dbc5fe8d9d6f38d6a679688beb99c09927748d3c
Alexande B <abobrikovich@gmail.com>
2019-05-26 11:54:27 +0300

    Remove RIDK installation

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -31,16 +31,9 @@ Get-ChocolateyUnzip -FileFullPath $idnitsArchivePath -Destination $packageSandbo
 $idnitsPath = -join($packageSandboxPath, $separator, [System.IO.Path]::GetFileNameWithoutExtension($idnitsArchive))
 Install-ChocolateyPath -PathToInstall $idnitsArchive
 
-Write-Host Installing RIDK...
-
-$RubyGem = "$Env:ChocolateyToolsLocation\ruby25\bin"
-$RidkProcess = Start-Process -PassThru -FilePath "$RubyGem\ridk.cmd" -ArgumentList "install 2 3"
-# Start-Process prevent Appveyour from hang
-Wait-Process -Id $RidkProcess.Id
-Update-SessionEnvironment
-
 Write-Host Installing gems...
 
+$RubyGem = "$Env:ChocolateyToolsLocation\ruby25\bin"
 & $RubyGem\gem.cmd install bundler
 & $RubyGem\gem.cmd install metanorma-cli -v $Env:ChocolateyPackageVersion
 
