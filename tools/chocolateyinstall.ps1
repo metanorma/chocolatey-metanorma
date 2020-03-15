62c9f4f7ed05ec370f60fdaf331751d7e3852264
Alexande B <abobrikovich@gmail.com>
2020-03-15 15:29:49 +0300

    #32 add idnits archive downloading logic

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -10,6 +10,27 @@ if (Get-Command "python" -errorAction SilentlyContinue) {
 	Write-Host Skip installing idnits and xml2rfc because no python was found
 }
 
+Write-Host Installing https://tools.ietf.org/tools/idnits/idnits-*.tar archive...
+
+$separator = [IO.Path]::DirectorySeparatorChar
+$packageSandboxPath = -join($env:ChocolateyInstall, $separator, "lib", $separator, "metanorma")
+
+$idnitsBaseUrl = "https://tools.ietf.org/tools/idnits/"
+$idnitsResponse = Invoke-WebRequest -Uri $idnitsBaseUrl -UseBasicParsing
+$idnitsArchive = $idnitsResponse.Links | Where-Object {$_.href -like "idnits-*"} | % { $_.href }
+
+$idnitsDownloadUrl = -join($idnitsBaseUrl, $idnitsArchive)
+$idnitsArchivePath = -join($packageSandboxPath, $separator, $idnitsArchive)
+Invoke-WebRequest -Uri $idnitsDownloadUrl -OutFile $idnitsArchivePath
+
+Write-Host Show downloaded file 
+Get-ChildItem -Path $packageSandboxPath
+
+Get-ChocolateyUnzip -FileFullPath $idnitsArchivePath -Destination $packageSandboxPath
+
+$idnitsPath = -join($packageSandboxPath, $separator, [System.IO.Path]::GetFileNameWithoutExtension($idnitsArchive))
+Install-ChocolateyPath -PathToInstall $idnitsArchive
+
 Write-Host Installing RIDK...
 
 $RubyGem = "$Env:ChocolateyToolsLocation\ruby25\bin"
