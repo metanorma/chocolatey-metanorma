6499bf7a875aab3a2973aa3b431c481550cb1f56
Alexande B <abobrikovich@gmail.com>
2021-03-11 19:40:49 +0300

    Revert hardcoded version from tools/chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -29,7 +29,7 @@ Install-ChocolateyPath -PathToInstall $idnitsArchive
 
 Write-Host Installing packed-mn...
 
-$metanormaUrl = "https://github.com/metanorma/packed-mn/releases/download/v1.4.5/metanorma-windows-x64.exe"
+$metanormaUrl = "https://github.com/metanorma/packed-mn/releases/download/v${Env:ChocolateyPackageVersion}/metanorma-windows-x64.exe"
 $metanormaPath = -join($Env:ChocolateyInstall, $separator, "bin", $separator, "metanorma.exe")
 Invoke-WebRequest -Uri $metanormaUrl -OutFile $metanormaPath
 
